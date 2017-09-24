87f0e620025652e49a268f261713b77cc767d1a4
SuvamPramanik <pramaniksuvam@yahoo.com>
2017-09-25 00:01:29 +0530

    Creating PendingIntent

---
+++ PendingIntent/app/src/main/java/com/android/pendingintent/MainActivity.java
@@ -0,0 +1,73 @@
+package com.android.pendingintent;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Button button1, button2;
+    private static final int NOTIFICATION_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get the button from the activity_main.xml using findViewByIId function
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Toast.makeText(this, "CREATE NOTIFICATION button clicked!", Toast.LENGTH_SHORT).show();
+                createNotification();
+                break;
+
+            case R.id.button2:
+                Toast.makeText(this, "CANCEL NOTIFICATION button clicked!", Toast.LENGTH_SHORT).show();
+                cancelNotification();
+                break;
+        }
+
+    }
+
+    private void createNotification() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/SuvamPramanik/Android-Tutorials/"));
+        // Creating a pending intent and wrapping our intent
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Create a notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentIntent(pendingIntent)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("Notification Title")
+                .setSubText("Tap to open athe tutorial")
+                .setContentText("Put your notification content here.");
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void cancelNotification() {
+
+        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+        manager.cancel(NOTIFICATION_ID);
+    }
+}
